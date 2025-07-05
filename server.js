4b7de55dde75cf25463937744284c1f56c3d0328
Harsh Bisht <harshbisht7456@gmail.com>
2025-07-05 22:49:55 +0530

    server.js

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
-// Explicitly serve sitemap.xml with correct Content-Type
-app.get('/sitemap.xml', (req, res) => {
-  res.set('Content-Type', 'application/xml');
-  res.sendFile(path.join(__dirname, 'public', 'sitemap.xml'));
-});
-
 // Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
 
